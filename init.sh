5bba0c6e5c1c75c7cb80381930bced2734eaabea
kyle-manganyi <46721650+kyle-manganyi@users.noreply.github.com>
2019-09-07 11:12:53 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/ma
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
-kubectl apply -f temp.yaml
 
 kubectl create serviceaccount tiller --namespace kube-system
 kubectl create clusterrolebinding tiller-role-binding --clusterrole cluster-admin --serviceaccount=kube-system:tiller
@@ -11,3 +10,5 @@ helm init --service-account tiller
 helm repo up
 
 helm install stable/prometheus-operator --name prometheus-operator --namespace temp
+
+kubectl apply -f temp.yaml
