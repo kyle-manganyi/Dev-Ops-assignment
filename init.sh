c43f7bcf7cc06188be6553332f1ca5c6dcd52ea5
Sudo-Kylies97 <manganyikyle@gmail.com>
2019-09-09 11:06:09 +0200

    .

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
+kubectl create namespace temp
 
 kubectl create serviceaccount tiller --namespace kube-system
 kubectl create clusterrolebinding tiller-role-binding --clusterrole cluster-admin --serviceaccount=kube-system:tiller
@@ -9,6 +6,6 @@ kubectl create clusterrolebinding tiller-role-binding --clusterrole cluster-admi
 helm init --service-account tiller
 helm repo up
 
-helm install stable/prometheus-operator --name prometheus-operator --namespace temp
+helm install --name prom-operator stable/prometheus-operator --namespace temp
 
 kubectl apply -f temp.yaml
